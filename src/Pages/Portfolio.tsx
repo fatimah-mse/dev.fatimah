ff51ce2b67007ed76fd828ccb4ea928d4056ff6b
fatimah-mse <fatimahmse83@gmail.com>
2025-06-21 01:16:16 +0300

    edit-scroll

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react"
 import icon from "../assets/Work Section/icon.png"
 import AllWorks from "../components/AllWorks"
 import CommonHero from "../components/CommonHero"
 
 export default function Portfolio() {
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }, [])
+    
     return (
         <>
             <CommonHero
