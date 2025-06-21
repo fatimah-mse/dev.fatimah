484d741840247c35d4871f93593662290608362c
fatimah-mse <fatimahmse83@gmail.com>
2025-06-21 18:07:22 +0300

    edit-nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
     }
 
     return (
-        <nav className={`4xl:px-[10%] fixed top-0 z-50 flex justify-between items-center ${header ? 'bg-myPrimary top-0 shadow-md' : 'bg-mybg'} w-full py-5 max-992:py-4 max-992:px-10 max-768:px-5 px-20`}>
+        <nav className={`4xl:px-[10%] fixed top-0 z-50 flex justify-between items-center ${header ? 'bg-myPrimary top-0 shadow-md' : 'bg-mybg'} transition-all duration-700 ease-in-out w-full py-5 max-992:py-4 max-992:px-10 max-768:px-5 px-20`}>
             <h1 className={`${header ? 'text-white' : 'text-myPrimary'} font-merienda text-2xl max-768:text-xl font-bold`}>Dev. Fatimah Mse</h1>
             <div className={` flex justify-between items-center max-992:w-full max-992:flex-col max-992:fixed max-992:top-0 max-992:right-0 max-992:h-screen transition-all duration-700 ease-in-out max-992:justify-evenly max-992:gap-0 ${header ? 'bg-myPrimary' : 'bg-mybg'} ${show ? 'translate-x-0' : 'max-992:translate-x-[200%]'}`}>
                 <h1 className={`${header ? 'text-white' : ''} font-merienda text-2xl hidden max-992:block`}>Dev. Fatimah Mse</h1>
@@ -48,7 +48,7 @@ export default function Navbar() {
                                     onClick={handleScrollToTop}
                                     to={e.link}
                                     className={({ isActive }) =>
-                                        `py-2.5 2xl:py-3.5 px-5 font-semibold ${header ? 'text-white' : ''} ${isActive ? 'text-white bg-mySecondary rounded-lg' : ''} `}
+                                        `py-2.5 2xl:py-3.5 px-5 font-semibold ${header ? 'text-white' : ''} ${isActive ? 'text-white bg-mySecondary rounded-lg' : ''} transition-all duration-700 ease-in-out`}
                                 >
                                     {e.item}
                                 </NavLink>
